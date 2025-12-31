c4b8ca9164db4525e5eb26884acaa0203dd36b47
0xvans <sercoleopard5@gmail.com>
2025-12-31 17:52:11 +0700

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -5,13 +5,13 @@ export async function GET() {
   <!DOCTYPE html>
   <html>
     <head>
-      <meta property="og:title" content="Based Apollo NFT" />
-      <meta property="og:description" content="BASED APOLLO NFT for Farcaster user." />
-      <meta property="og:image" content="https://based-apollo.vercel.app/splash.png" />
+      <meta property="og:title" content="Farcaster XBT NFT" />
+      <meta property="og:description" content="Farcaster XBT NFT for Farcaster user." />
+      <meta property="og:image" content="https://fxbt.vercel.app/splash.png" />
       <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://based-apollo.vercel.app/splash.pngg" />
+      <meta property="fc:frame:image" content="https://fxbt.vercel.app/splash.pngg" />
       <meta property="fc:frame:button:1" content="Mint Now" />
-      <meta property="fc:frame:post_url" content="https://based-apollo.vercel.app/" />
+      <meta property="fc:frame:post_url" content="https://fxbt.vercel.app/" />
     </head>
     <body></body>
   </html>`;
